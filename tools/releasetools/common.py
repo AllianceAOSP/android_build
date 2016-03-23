8b200712b35a42904d4ddf7663870f3738e51715
Morningstar <admirethedistance@gmail.com>
2016-03-22 22:33:58 -0600

    New icon in updater-script

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1208,7 +1208,7 @@ class BlockDifference(object):
     if not self.src:
       # write the output unconditionally
       script.Print(" ")
-      script.Print("Flashing System..")
+#      script.Print("Installing Alliance...")
     else:
       script.Print("Patching %s image after verification." % (self.partition,))
 
